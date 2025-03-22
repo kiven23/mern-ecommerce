2c38ff5bacea705b324d93e65cea148d287d2691
stevefox_linux <calimlim.steven@gmail.com>
2025-03-22 13:56:48 +0800

    Update storage.js

--- server/utils/storage.js
+++ server/utils/storage.js
@@ -1,3 +1,45 @@
+const axios = require('axios');
+const FormData = require('form-data');
+
+exports.s3Upload = async (image) => {
+  try {
+    let imageUrl = '';
+    let imageKey = '';
+
+    if (!image) {
+      console.warn('No image provided');
+      return { imageUrl, imageKey };
+    }
+
+    // Prepare form-data
+    const formData = new FormData();
+    formData.append('file', image.buffer, {
+      filename: image.originalname,
+      contentType: image.mimetype,
+    });
+
+    // Upload to your backend acting as S3 clone
+    const response = await axios.post(
+      'https://colossal-sun-alpaca.glitch.me/upload',
+      formData,
+      { headers: formData.getHeaders() }
+    );
+
+    // Handle response
+    if (response.data && response.data.url) {
+      imageUrl = response.data.url;
+      imageKey = response.data.url; // or any key logic you like
+    }
+
+    return { imageUrl, imageKey };
+  } catch (error) {
+    console.error('Upload Error:', error.message);
+    return { imageUrl: '', imageKey: '' };
+  }
+};
+
+
+
 // const AWS = require('aws-sdk');
 
 // const keys = require('../config/keys');
@@ -37,77 +79,79 @@
 //   }
 // };
 
-//const AWS = require('aws-sdk');
-//
-//const keys = require('../config/keys');
-//
-//exports.s3Upload = async image => {
-//  try {
-//    let imageUrl = '';
-//    let imageKey = '';
-//
-//    if (!keys.aws.accessKeyId) {
-//      console.warn('Missing aws keys');
-//    }
-//
-//    if (image) {
-//      const s3bucket = new AWS.S3({
-//        accessKeyId: keys.aws.accessKeyId,
-//        secretAccessKey: keys.aws.secretAccessKey,
-//        region: keys.aws.region
-//      });
-//
-//      const params = {
-//        Bucket: keys.aws.bucketName,
-//        Key: image.originalname,
-//        Body: image.buffer,
-//        ContentType: image.mimetype
-//      };
-//
-//      const s3Upload = await s3bucket.upload(params).promise();
-//
-//      imageUrl = s3Upload.Location;
-//      imageKey = s3Upload.key;
-//    }
-//
-//    return { imageUrl, imageKey };
-//  } catch (error) {
-//    return { imageUrl: '', imageKey: '' };
-//  }
-//};
+// //const AWS = require('aws-sdk');
+// //
+// //const keys = require('../config/keys');
+// //
+// //exports.s3Upload = async image => {
+// //  try {
+// //    let imageUrl = '';
+// //    let imageKey = '';
+// //
+// //    if (!keys.aws.accessKeyId) {
+// //      console.warn('Missing aws keys');
+// //    }
+// //
+// //    if (image) {
+// //      const s3bucket = new AWS.S3({
+// //        accessKeyId: keys.aws.accessKeyId,
+// //        secretAccessKey: keys.aws.secretAccessKey,
+// //        region: keys.aws.region
+// //      });
+// //
+// //      const params = {
+// //        Bucket: keys.aws.bucketName,
+// //        Key: image.originalname,
+// //        Body: image.buffer,
+// //        ContentType: image.mimetype
+// //      };
+// //
+// //      const s3Upload = await s3bucket.upload(params).promise();
+// //
+// //      imageUrl = s3Upload.Location;
+// //      imageKey = s3Upload.key;
+// //    }
+// //
+// //    return { imageUrl, imageKey };
+// //  } catch (error) {
+// //    return { imageUrl: '', imageKey: '' };
+// //  }
+// //};
 
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
-exports.localUpload = async (image) => {
-  try {
-    let imageUrl = '';
-    let imageKey = '';
+// exports.localUpload = async (image) => {
+//   try {
+//     let imageUrl = '';
+//     let imageKey = '';
      
-    if (image) {
-      const uploadsDir = '/var/task/server/utils/';
+//     if (image) {
+//       const uploadsDir = '/var/task/server/utils/';
       
-      // Auto-create uploads folder kung wala pa
-      if (!fs.existsSync(uploadsDir)) {
-        fs.mkdirSync(uploadsDir, { recursive: true });
-      }
+//       // Auto-create uploads folder kung wala pa
+//       if (!fs.existsSync(uploadsDir)) {
+//         fs.mkdirSync(uploadsDir, { recursive: true });
+//       }
 
-      const fileName = `${Date.now()}-${image.originalname}`;
-      const filePath = path.join(uploadsDir, fileName);
+//       const fileName = `${Date.now()}-${image.originalname}`;
+//       const filePath = path.join(uploadsDir, fileName);
 
-      // Save image buffer sa target folder
-      fs.writeFileSync(filePath, image.buffer);
+//       // Save image buffer sa target folder
+//       fs.writeFileSync(filePath, image.buffer);
 
-      // Sample public URL kung iseserve mo via frontend
-      imageUrl = `/${fileName}`;
-      imageKey = fileName;
-    }
-    console.log(imageUrl)
-    return { imageUrl, imageKey };
-  } catch (error) {
-    console.error(error);
-    return { imageUrl: '', imageKey: '' };
-  }
-};
+//       // Sample public URL kung iseserve mo via frontend
+//       imageUrl = `/${fileName}`;
+//       imageKey = fileName;
+//     }
+//     console.log(imageUrl)
+//     return { imageUrl, imageKey };
+//   } catch (error) {
+//     console.error(error);
+//     return { imageUrl: '', imageKey: '' };
+//   }
+// };
+
+
 
 
