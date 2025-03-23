30c005c272d6c24b5f250b4ed51d6f5766be300e
kiven23 <calimlim.steven@gmail.com>
2025-03-23 10:46:17 +0800

    updated

--- client/app/containers/Homepage/index.js
+++ client/app/containers/Homepage/index.js
@@ -35,18 +35,20 @@ class Homepage extends React.PureComponent {
               </CarouselSlider>
             </div>
           </Col>
-          <Col xs='12' lg='3' className='order-lg-1 mb-3 px-3 px-md-2'>
-            <div className='d-flex flex-column h-100 justify-content-between'>
-              <img src='/images/banners/banner-2.jpg' className='mb-3' />
-              <img src='/images/banners/banner-5.jpg' />
+          <Col xs='12' lg='3' className='order-lg-2 mb-1 px-1 px-md-1'>
+            <div className='d-flex flex-column h-70 justify-content-between'>
+              <img src='/images/banners/banner-10.webp' className='mb-1' />
+              <img src='/images/banners/banner-11.jpg' />
             </div>
           </Col>
-          <Col xs='12' lg='3' className='order-lg-3 mb-3 px-3 px-md-2'>
-            <div className='d-flex flex-column h-100 justify-content-between'>
-              <img src='/images/banners/banner-2.jpg' className='mb-3' />
-              <img src='/images/banners/banner-6.jpg' />
+          <Col xs='12' lg='3' className='order-lg-2 mb-1 px-1 px-md-1'>
+            <div className='d-flex flex-column h-70 justify-content-between'>
+              <img src='/images/banners/banner-13.avif' className='mb-1' />
+              <img src='/images/banners/banner-12.jpg' />
             </div>
+
           </Col>
+
         </Row>
       </div>
     );
