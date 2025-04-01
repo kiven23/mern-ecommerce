0279da870b8b0ad53cdbd5fe40983a65dbbe3f58
kiven23 <calimlim.steven@gmail.com>
2025-04-01 22:17:38 +0800

    updated

--- client/app/containers/Homepage/index.js
+++ client/app/containers/Homepage/index.js
@@ -37,14 +37,14 @@ class Homepage extends React.PureComponent {
           </Col>
           <Col xs='12' lg='3' className='order-lg-2 mb-1 px-1 px-md-1'>
             <div className='d-flex flex-column h-70 justify-content-between'>
-              <img src='/images/banners/banner-10.webp' className='mb-1' />
-              <img src='/images/banners/banner-11.jpg' />
+              <img src='/images/banners/perfume/1.png' className='mb-1' />
+              <img src='/images/banners/perfume/2.png' />
             </div>
           </Col>
           <Col xs='12' lg='3' className='order-lg-2 mb-1 px-1 px-md-1'>
             <div className='d-flex flex-column h-70 justify-content-between'>
-              <img src='/images/banners/banner-13.avif' className='mb-1' />
-              <img src='/images/banners/banner-12.jpg' />
+              <img src='/images/banners/perfume/3.png' className='mb-1' />
+              <img src='/images/banners/perfume/4.png' />
             </div>
 
           </Col>
