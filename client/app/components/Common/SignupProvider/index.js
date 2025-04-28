1e9c17cbd39eaa13cace069bc218410fb4c8781e
kiven23 <calimlim.steven@gmail.com>
2025-04-28 23:40:29 +0800

    updated

--- client/app/components/Common/SignupProvider/index.js
+++ client/app/components/Common/SignupProvider/index.js
@@ -12,15 +12,15 @@ import { API_URL } from '../../../constants';
 const SignupProvider = () => {
   return (
     <div className='signup-provider'>
-      <a href={`${API_URL}/auth/google`} className='mb-2 google-btn'>
+      {/* <a href={`${API_URL}/auth/google`} className='mb-2 google-btn'>
         <GoogleIcon />
         <span className='btn-text'>Login with Google</span>
-      </a>
+      </a> */}
 
-      <a href={`${API_URL}/auth/facebook`} className='facebook-btn'>
+      {/* <a href={`${API_URL}/auth/facebook`} className='facebook-btn'>
         <FacebookIcon />
         <span className='btn-text'>Login with Facebook</span>
-      </a>
+      </a> */}
     </div>
   );
 };
